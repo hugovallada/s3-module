1e425c85ee4cce4e88d9a22a3b72454274985be0
Hugo Vallada <valladahugo@gmail.com>
2022-10-10 06:25:37 -0300

    Ajustes

--- infra/main.tf
+++ infra/main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "hugovallada-tfstate"
-    key    = "tf-state/[nome_do_projeto]/terraform.tfstate" //TODO: Substituir pelo nome do projeto
+    key    = "tf-state/${timestamp()}/terraform.tfstate"
     region = "sa-east-1"
   }
 }
